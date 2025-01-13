90da2a7e407bec8d5ce0879d2d54d7a8707660b1
KMaleesha <maleeshakumarasinghe@gmail.com>
2025-01-13 12:19:59 +0530

    thread practice

--- src/ConsistencyProblem.java
+++ src/ConsistencyProblem.java
@@ -1,8 +1,11 @@
 public class ConsistencyProblem {
     public static void main(String[] args) {
-        Printer printer = new Printer();
-        PrinterThread printerThread1 = new PrinterThread(printer, "Printer 1");
-        PrinterThread printerThread2 = new PrinterThread(printer, "Printer 2");
+//        Printer printer = new Printer();
+//        PrinterThread printerThread1 = new PrinterThread(printer, "Printer 1");
+//        PrinterThread printerThread2 = new PrinterThread(printer, "Printer 2");
+
+        PrinterThread printerThread1 = new PrinterThread("Printer 1");
+        PrinterThread printerThread2 = new PrinterThread("Printer 2");
 
         printerThread1.start();
         printerThread2.start();
@@ -24,41 +27,67 @@ public class ConsistencyProblem {
 //}
 
 //synchronized block
-class Printer{
-    public void print(String name){
+//class Printer{
+//    public void print(String name){
+//
+//        for(int i=0; i<10; i++){
+//            System.out.println("my name is "+name);
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        synchronized (this){
+//            for(int i=0; i<10; i++){
+//                System.out.println(name);
+//                try {
+//                    Thread.sleep(500);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
+//}
 
+//class PrinterThread extends Thread{
+//    Printer p;
+//    String name;
+//
+//    public PrinterThread(Printer p, String name){
+//        this.p = p;
+//        this.name = name;
+//    }
+//
+//    public void run(){
+//        this.p.print(name);
+//    }
+//}
+
+//static synchronized method
+class Printer{
+    public synchronized static void print(String name){
         for(int i=0; i<10; i++){
-            System.out.println("my name is "+name);
+            System.out.println(name);
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
-        synchronized (this){
-            for(int i=0; i<10; i++){
-                System.out.println(name);
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
     }
 }
 
 class PrinterThread extends Thread{
-    Printer p;
     String name;
 
-    public PrinterThread(Printer p, String name){
-        this.p = p;
+    public PrinterThread(String name){
         this.name = name;
     }
 
     public void run(){
-        this.p.print(name);
+        Printer.print(name);
     }
 }
